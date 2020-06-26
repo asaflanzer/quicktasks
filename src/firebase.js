172760cd5c697ad271b4869d42188644a0e73544
Asaf Lanzer <asaflanzer@gmail.com>
2020-06-26 14:48:17 +0300

    secure firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,16 @@
 import firebase from 'firebase/app';
 import { firestore } from 'firebase/firestore';
 
+const fbConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
 const firebaseConfig = firebase.initializeApp({
   apiKey: 'AIzaSyCCG4tvuRuLan9DaWP3p28LEznpzmqC1NI',
   authDomain: 'todoist-app-68f61.firebaseapp.com',
